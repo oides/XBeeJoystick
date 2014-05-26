854d0f234abb3e4c7b8a6ed8c8e59693e866d2e5
eudes.andrade <eudes.andrade@gmail.com>
2014-05-25 21:11:22 -0300

    Teste de commit

--- client/clientEngine/Joystick/src/oidaslabs/library/java/joystick/PressD.java
+++ client/clientEngine/Joystick/src/oidaslabs/library/java/joystick/PressD.java
@@ -11,7 +11,7 @@ public class PressD {
 		joystick.openConnection("/dev/ttyUSB0", new int[] { 0x00, 0x13, 0xA2, 0x00, 0x40, 0x69, 0xDD, 0x12 });
 
 		joystick.pressD();
-
+		
 		joystick.closeConnection();
 
 	}
